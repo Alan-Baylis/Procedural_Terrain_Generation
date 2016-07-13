4b4ca8339a2c5feaf225bf2210a9eac54e0a1e08
ilaumarius <ilaumarius05@gmail.com>
2016-07-13 15:04:55 +0300

    2D texture map with perlin from Mathf

---
+++ Assets/Editor/MapGeneratorEditor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(MapGenerator))]
+public class NewBehaviourScript : Editor
+{
+	public override void OnInspectorGUI()
+	{
+		MapGenerator mapGen = (MapGenerator)target;
+
+		DrawDefaultInspector();
+
+		if (GUILayout.Button("Generate"))
+		{
+			mapGen.GenerateMap();
+		}
+	}
+}
